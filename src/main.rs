43a1c81ae9c7d121643d9c6c28fac0d391b3a653
bluesboynix <bluesboychaosnix@gmail.com>
2026-09-03 21:07:11 +0530

    change fullscreen to false

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ slint::include_modules!();
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let window = MainWindow::new()?;
 
+    window.window().set_maximized(false);
+
     window.run()?;
 
     Ok(())
