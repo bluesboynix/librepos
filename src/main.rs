e3ee42cb8d138068a7e968f8c940cbcc0107c5c8
bluesboynix <bluesboychaosnix@gmail.com>
2026-09-03 17:21:29 +0530

    Create frist LibrePOS window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+slint::include_modules!();
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let window = MainWindow::new()?;
+
+    window.run()?;
+
+    Ok(())
 }
